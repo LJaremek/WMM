c6bdd12dfb4801155d6c880326e4dcc05cc039c1
Lukasz Jaremek <Jaremek.Lukasz@gmail.com>
2022-06-09 19:25:00 +0200

    try doing something with phong

--- lab_08/src/main.py
+++ lab_08/src/main.py
@@ -1,7 +1,7 @@
 import moderngl_window
 
-# from base_window import BaseWindowConfig
 from robot_window import RobotWindow
+# from phong_window import PhongWindow
 
 if __name__ == '__main__':
     moderngl_window.run_window_config(RobotWindow)
--- lab_08/src/phong_window.py
+++ lab_08/src/phong_window.py
@@ -1,27 +1,34 @@
 import moderngl
-from pyrr import Matrix44
+from pyrr import Matrix44, Vector4
 
 from base_window import BaseWindowConfig
 
 
 class PhongWindow(BaseWindowConfig):
-
     def __init__(self, **kwargs):
         super(PhongWindow, self).__init__(**kwargs)
+        self._colour = Vector4((0.75, 0.0, 1.0, 1.0))
 
     def init_shaders_variables(self):
-        pass
-        # TODO: Init shader variables
+        self._sphere = self.load_scene("sphere.obj")
+        self._lookat = self.program["lookat"]
 
     def render(self, time: float, frame_time: float):
         self.ctx.clear(1.0, 1.0, 1.0, 0.0)
         self.ctx.enable(moderngl.DEPTH_TEST | moderngl.CULL_FACE)
 
-        proj = Matrix44.perspective_projection(45.0, self.aspect_ratio, 0.1, 1000.0)
+        proj = Matrix44.perspective_projection(
+            45.0,
+            self.aspect_ratio,
+            0.1,
+            1000.0
+            )
+
         lookat = Matrix44.look_at(
             (3.0, 1.0, -5.0),
             (0.0, 0.0, 1.0),
             (0.0, 0.0, 1.0),
         )
-        # TODO: Write variables to shader
+
+        self._lookat.write(lookat.astype("f4"))
         self.vao.render()
